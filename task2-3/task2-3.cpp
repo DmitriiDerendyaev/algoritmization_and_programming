f72b675d7acd12750fa88839b87971641c68ce16
Dmitrii Derendiaev <dimonsterus007@ya.ru>
2021-09-30 19:38:57 +0300

    good task1_2-3

--- task2-3/task2-3.cpp
+++ task2-3/task2-3.cpp
@@ -1,9 +1,48 @@
-// task2-3.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <cmath>
+#include <stdio.h>
+
+using namespace std;
+float x1, x2, b, c;
+
+
+void task1(float b, float c, float* x1, float* x2)
+{
+	*x1 = (-b + sqrt(pow(b, 2) - 4 * c)) / 2;
+	*x2 = (-b - sqrt(pow(b, 2) - 4 * c)) / 2;
+}
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int z;
+	cin >> z;
+	switch (z)
+	{
+	case 1:
+	{
+		cout << "B:";
+		cin >> b;
+		cout << "C:";
+		cin >> c;
+		float xx1, xx2;
+		task1(b, c, &xx1, &xx2);
+		cout << "X1:" << xx1 << "; X2:" << xx2;
+	}
+	}
+
+	return 0;
 }
+//#include <iostream>
+//#include <cmath>
+//
+//int main()
+//{
+//	float x1, x2, d, a, b, c;
+//
+//	std::cin >> a >> b >> c;
+//
+//	d = pow(b, 2) - 4 * a * c;
+//
+//	if d < 0 :
+//}

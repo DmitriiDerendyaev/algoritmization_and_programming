c191a44670709162239d4e509586d136d57aa737
Dmitrii Derendiaev <dimonsterus007@ya.ru>
2021-10-10 03:57:21 +0300

    task_for in subprogramm

--- task4-5/task4-5.cpp
+++ task4-5/task4-5.cpp
@@ -2,45 +2,35 @@
 
 using namespace std;
 
-int i, j, n, buf, array[] = {7,5,4,6,1,2,3}, array_sort[7];
+int i, j, n, buf, array1[] = {7,5,4,6,1,2,3};
 
-void task1(int array, int* array_sort)
+//void printing(int array1[])
+//{
+//	for (int i = 0; i < sizeof(array1); i++)
+//		cout << array1[i] << " ";
+//
+//}
+
+void task_for(/*int array1[]*/)
 {
-	for (int i = 0; i < n - 1; i++)
-	{
-		for (int j = 0; j < n - 1 - i; j++)
-		{
-			if (array[j] > array[j + 1])
+	n = sizeof(array1);
+	for(int i = 0; i < n - 1; i++)
+		for (int j = 0; j < n - i - 1; j++)
+			if (array1[i] > array1[i + 1])
 			{
-				buf = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = buf;
+				buf = array1[i];
+				array1[i] = array1[i + 1];
+				array1[i + 1] = buf;
 			}
-		}
-	}
-	for (i = 0; i < n; i++)
-	{
-		array_sort[i] = array[i];
-	}
-}
 
+	for (int i = 0; i < n; i++)
+		cout << array1[i];
+	//printing(array1);
+}
 
 int main()
 {
-	int z; 
-	cin >> z;
-
-	switch (z)
-	{
-	case 1:
-	{
-		task1(array, &array_sort);
-	}
-	}
-	
-
-
-
+	task_for();
 
 	return 0;
 }

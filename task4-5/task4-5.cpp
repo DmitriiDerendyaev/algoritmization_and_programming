6c68567839d99fff617ea8b7b4cb275352b84b98
Dmitrii Derendiaev <dimonsterus007@ya.ru>
2021-10-10 04:01:06 +0300

    in main programm

--- task4-5/task4-5.cpp
+++ task4-5/task4-5.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-int i, j, n, buf, array1[] = {7,5,4,6,1,2,3};
+//int i, j, n, buf, array1[] = {7,5,4,6,1,2,3};
 
 //void printing(int array1[])
 //{
@@ -11,10 +11,30 @@ int i, j, n, buf, array1[] = {7,5,4,6,1,2,3};
 //
 //}
 
-void task_for(/*int array1[]*/)
+//void task_for(/*int array1[]*/)
+//{
+//	n = sizeof(array1);
+//	for(int i = 0; i < n - 1; i++)
+//		for (int j = 0; j < n - i - 1; j++)
+//			if (array1[i] > array1[i + 1])
+//			{
+//				buf = array1[i];
+//				array1[i] = array1[i + 1];
+//				array1[i + 1] = buf;
+//			}
+//
+//	for (int i = 0; i < n; i++)
+//		cout << array1[i];
+//	//printing(array1);
+//}
+
+int main()
 {
+
+	int i, j, n, buf, array1[] = { 7,5,4,6,1,2,3 };
+
 	n = sizeof(array1);
-	for(int i = 0; i < n - 1; i++)
+	for (int i = 0; i < n - 1; i++)
 		for (int j = 0; j < n - i - 1; j++)
 			if (array1[i] > array1[i + 1])
 			{
@@ -24,13 +44,8 @@ void task_for(/*int array1[]*/)
 			}
 
 	for (int i = 0; i < n; i++)
-		cout << array1[i];
-	//printing(array1);
-}
-
-int main()
-{
-	task_for();
-
+		cout << array1[i] << ".";
+	//task_for();
+	cout << "Do you exist?";
 	return 0;
 }

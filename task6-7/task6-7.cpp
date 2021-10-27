3feabec842f44e3fa0ae465cf145df50936c6482
Dmitrii Derendiaev <dimonsterus007@ya.ru>
2021-10-27 21:40:48 +0300

    added graf and comment about function in code(1/2)

--- task6-7/task6-7.cpp
+++ task6-7/task6-7.cpp
@@ -1,36 +1,38 @@
-#include <iostream>
+#include <iostream>//подключение необходимых библиотек
 #include <vector>
 #include <algorithm>
 
-using namespace std;
+using namespace std;//объ€вение стандартного пространства имен
 
-vector<int> vec1;
+vector<int> vec1;//объ€вление переменных
 int n;
 float k = 1.247;
 
 
-void reading(void)
+void reading(void)//инициализации функции заполнени€ вектора(с клавиатуры)
 {
-//again:
-	int el;
-	do
+	int el;//объ€вление промежуточной переменной, куда будет буферно записыватьс€ переменна€ дл€ очередного элемента вектора
+	while (cin >> el)//начало цикла while и работа его до тех пор, пока ввод€тс€ значени€ с клавиатуры(Stop: Ctrl+Z) - Ќ≈ –≈ ќћ≈Ќƒ”≈“—я
 	{
-		vec1.push_back(el);
-	} while (cin >> el and el != 0);
-}
-
-void printing(void)
-{
-	for (int i = 0; i < vec1.size(); i++)
-	{
-		cout << vec1[i] << " ";
+		if (el != 0)//конец строки дл€ опеределенной задачи по символу 0
+			vec1.push_back(el);//внесение очередного элемента в конец вектора
+		else
+			break;//если был нажат 0, то выход из подпрограммы записи вектора
 	}
-	cout << endl;
 }
 
-void sort_for(void)
+void printing(void)//инициализаци€ функции печати/вывода элементов на экран
 {
-	cout << "When finished typing, press CTRL + Z:" << endl;
+	for (int i = 0; i < vec1.size(); i++)//цикл вывода элементов на экран с индексом от начала до конца вектора(по размеру массива)
+	{
+		cout << vec1[i] << " ";//печать значени€
+	}
+	cout << endl;//перенос строки
+}
+
+void sort_for(void)//инициализаци€ функции сортировки массива с помощью цикла for
+{
+	cout << "When finished typing, press CTRL + Z or enter 0 to contine next task:" << endl;//предупреждение
 	reading();
 	n = vec1.size();
 	for (int step = n - 1; step >= 1; step /= k)

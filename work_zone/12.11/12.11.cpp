b40e833e8971f3b0f1523a71bf44920f1cefa27c
Dmitrii Derendiaev <dimonsterus007@ya.ru>
2021-11-12 14:57:00 +0300

    added counter

--- work_zone/12.11/12.11.cpp
+++ work_zone/12.11/12.11.cpp
@@ -3,24 +3,37 @@
 int main()
 {
 
-    int i, j, step, n, mas[] = { 8,5,4,2,1 }, mas1[] = { 8,5,4,2,1 };
+    int i, j, step, n, mas[] = { 8,5,4,2,1, 45,56,89,16,10,67,90,98,198 }, mas1[] = { 8,5,4,2,1, 45,56,89,16,10,67,90,98,198 };
+    int count_swap = 0, count_cycle = 0;
     float k = 1.274;
     n = sizeof(mas) / sizeof(int);
     step = n - 1;
-
-    while (step >= 1)
+    int flag = 0;
+    while (step > 1 || flag == 1)
     {
+        flag = 0;
+        count_cycle++;
         for (int i = 0; i < n - step; i++)
         {
+            count_cycle++;
             if (mas[i] > mas[i + step])
             {
+                flag = 1;
+                count_swap++;
                 mas[i] = mas[i] ^ mas[i + step];
                 mas[i + step] = mas[i] ^ mas[i + step];
                 mas[i] = mas[i] ^ mas[i + step];
             }
         }
         step /= k;
+        //flag = 0;
+        if (step == 0)
+        {
+            step = 1;
+        }
     }
+
+    printf("%i %i\n", count_swap, count_cycle);
 
     for (i = 0; i < n; i++)
     {
@@ -29,12 +42,18 @@ int main()
 
  //-----------------------------------------------------------
 
+    count_swap = 0;
+    count_cycle = 0;
+
     for (j = 0; j < n - 1; j++)
     {
+        count_cycle++;
         for (i = 0; i < n - j - 1; i++)
         {
+            count_cycle++;
             if (mas[i] > mas[i + 1])
             {
+                count_swap++;
                 mas[i] = mas[i] ^ mas[i + 1];
                 mas[i + 1] = mas[i] ^ mas[i + 1];
                 mas[i] = mas[i] ^ mas[i + 1];
@@ -43,6 +62,8 @@ int main()
     }
 
     printf("\n");
+
+    printf("%i %i\n", count_swap, count_cycle);
 
     for (i = 0; i < n; i++)
     {

d2c4145511f5befffd554eab5ada3fe004ab1d9a
Dmitrii Derendiaev <dimonsterus007@ya.ru>
2021-12-04 13:33:06 +0300

    good tas14-15 without comments

--- task14-15/task14-15.cpp
+++ task14-15/task14-15.cpp
@@ -20,6 +20,7 @@ int main()
                 continue;
 
         }
+        
         i = j++;
     }    
 
@@ -28,15 +29,36 @@ int main()
         cout << mas[i] << ' ';
     }
 
-    cout << endl;
+    cout << endl  << endl;
 
 
 
     //------------------------------------------------------
 
-    int N, j = 2, i = 1, buf;
+    int n;
+    j = 2, i = 1, buf;
     //int *mas = new int[7];
     int mas2[]{ 6, 4, 1, 5, 3, 7, 2 };
+    n = sizeof(mas2) / sizeof(int);
+
+    for(i = j; i < n; j++)
+    {
+        if (mas2[i - 1] > mas2[i])
+        {
+            buf = mas2[i];
+            mas2[i] = mas2[i - 1];
+            mas2[i - 1] = buf;
+            i--;
+            if (i > 0)
+                continue;
+        }
+        i = j++;
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        cout << mas[i] << ' ';
+    }
 
 
     return 0;
